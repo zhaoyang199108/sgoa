8970c4094acaa9de9164c61a6f76b0331036aa93
Administrator <Administrator@ULOE3T20M9MZ3BS>
2016-10-13 16:36:32 +0800

    清包

--- sgoa/src/main/java/com/bcqsoft/sgoa/mvc/controller/index/AppLoginController.java
+++ sgoa/src/main/java/com/bcqsoft/sgoa/mvc/controller/index/AppLoginController.java
@@ -1,8 +1,6 @@
 package com.bcqsoft.sgoa.mvc.controller.index;
 
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
@@ -11,7 +9,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.bcqsoft.sgoa.common.util.MD5Util;
