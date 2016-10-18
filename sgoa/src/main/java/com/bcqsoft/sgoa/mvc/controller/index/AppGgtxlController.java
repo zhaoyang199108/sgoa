a839893025568d425ea56e65b730c238dadb640f
Administrator <Administrator@ULOE3T20M9MZ3BS>
2016-10-18 14:14:37 +0800

    公共通讯录接口

--- sgoa/src/main/java/com/bcqsoft/sgoa/mvc/controller/index/AppGgtxlController.java
+++ sgoa/src/main/java/com/bcqsoft/sgoa/mvc/controller/index/AppGgtxlController.java
@@ -1,8 +1,15 @@
 package com.bcqsoft.sgoa.mvc.controller.index;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.support.incrementer.HsqlMaxValueIncrementer;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -11,6 +18,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import com.bcqsoft.sgoa.core.security.SecurityUtils;
 import com.bcqsoft.sgoa.dao.ggtxl.dataobject.Ggtxl;
 import com.bcqsoft.sgoa.dao.ggtxl.dataobject.GgtxlPage;
+import com.bcqsoft.sgoa.mvc.controller.index.util.GgtxlRes;
 import com.bcqsoft.sgoa.mvc.form.ggtxl.GgtxlForm;
 import com.bcqsoft.sgoa.service.ggtxl.GgtxlService;
 
@@ -39,17 +47,41 @@ public class AppGgtxlController {
 	 * @Author Bcqsoft.com cql
 	 * @Date 2012-05-02
 	 */
-	@RequestMapping(value = "/home/ggtxl/txlgg_list.htm", method = RequestMethod.GET)
+	@RequestMapping(value = "/home/ggtxl_search_list.htm", method = RequestMethod.GET)
 	@ResponseBody
-	public List<Ggtxl> selectTxlggListByPage(GgtxlForm form,String deptId) {
+	public List<GgtxlRes>  selectGgtxlSearchList(
+			HttpServletRequest request, HttpServletResponse response) {
+		List<Map<String, Object>>  result = new ArrayList<Map<String,Object>>();
 		GgtxlPage ggtxlPage = new GgtxlPage(); // 分页对象
-		setSearchKey(form, ggtxlPage); // 设置页面中的查询条件
-		ggtxlPage.setIsmm("Y");
-		ggtxlPage.setTypeId(deptId);
-		List<Ggtxl> ggtxlList = ggtxlService.getGgtxlAllList(ggtxlPage).getGgtxlList();
-		// 跳转至公共通讯录申领表列表页面
-		return ggtxlList;
+		String retCode = "";
+		String message = "";
+		GgtxlPage page = ggtxlService.getGgtxlInfoSearchList(ggtxlPage);
+		List<Ggtxl> ggtxlList = page.getGgtxlList();
+		Map<String, Object> map = new HashMap<String, Object>();
+		Map<String, Object> mapRes = new HashMap<String, Object>();
+		List<Ggtxl> list = new ArrayList<Ggtxl>();
+		for(Ggtxl i : ggtxlList){
+			map.put(i.getTypeId(), null);
+		}
+		List<Object> lis = new ArrayList<Object>();
+		List<GgtxlRes> ggResList = new ArrayList<GgtxlRes>();
+		for(String t : map.keySet()){
+			
+			GgtxlRes ggtxlRes = new GgtxlRes();
+				List<Ggtxl> ggList = new ArrayList<Ggtxl>();
+				for(Ggtxl i:ggtxlList){
+					if(i.getTypeId().equals(t)){
+						ggList.add(i);
+						ggtxlRes.name=i.getTypeName();
+					}
+				}
+			
+			ggtxlRes.ggtxl = ggList;
+			ggResList.add(ggtxlRes);
+		}
+		return ggResList;
 	}
+
 	/**
 	 * 公共通讯录申领表列表页面设置查询条件
 	 * 
---
+++ sgoa/src/main/java/com/bcqsoft/sgoa/mvc/controller/index/util/GgtxlRes.java
@@ -0,0 +1,10 @@
+package com.bcqsoft.sgoa.mvc.controller.index.util;
+
+import java.util.List;
+
+import com.bcqsoft.sgoa.dao.ggtxl.dataobject.Ggtxl;
+
+public class GgtxlRes {
+	public String name;
+	public List<Ggtxl> ggtxl;
+}
