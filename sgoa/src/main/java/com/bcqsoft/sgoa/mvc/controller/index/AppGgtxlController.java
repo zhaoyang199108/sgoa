e721d4d315306a658fd5d7670f653321c42b53b3
Administrator <Administrator@ULOE3T20M9MZ3BS>
2016-10-19 10:04:37 +0800

    公共通讯录接口

--- sgoa/src/main/java/com/bcqsoft/sgoa/mvc/controller/index/AppGgtxlController.java
+++ sgoa/src/main/java/com/bcqsoft/sgoa/mvc/controller/index/AppGgtxlController.java
@@ -9,7 +9,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.support.incrementer.HsqlMaxValueIncrementer;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -44,12 +43,12 @@ public class AppGgtxlController {
 	 * @param map
 	 * @return
 	 * 
-	 * @Author Bcqsoft.com cql
-	 * @Date 2012-05-02
+	 * @Author zy
+	 * @Date 2016-10-19
 	 */
 	@RequestMapping(value = "/home/ggtxl_search_list.htm", method = RequestMethod.GET)
 	@ResponseBody
-	public List<GgtxlRes>  selectGgtxlSearchList(
+	public Map<String,Object> selectGgtxlSearchList(
 			HttpServletRequest request, HttpServletResponse response) {
 		List<Map<String, Object>>  result = new ArrayList<Map<String,Object>>();
 		GgtxlPage ggtxlPage = new GgtxlPage(); // 分页对象
@@ -79,17 +78,20 @@ public class AppGgtxlController {
 			ggtxlRes.ggtxl = ggList;
 			ggResList.add(ggtxlRes);
 		}
-		return ggResList;
+		Map<String,Object> resMap = new HashMap<String, Object>();
+		retCode = ggResList == null?"1":"0";
+		message = ggResList == null?"取得失败":"取得成功";
+		resMap.put("retCode", retCode);
+		resMap.put("message", message);
+		resMap.put("data",ggResList);
+		return resMap;
+		//return ggResList;
 	}
 
 	/**
 	 * 公共通讯录申领表列表页面设置查询条件
-	 * 
-	 * @param form
-	 * @param GgtxlPage
-	 * 
-	 * @Author cql
-	 * @Date 2012-05-02
+	 * @Author zy
+	 * @Date 2016-10-17
 	 */
 	private void setSearchKey(GgtxlForm form, GgtxlPage ggtxlPage) {
 		ggtxlPage.setLoginId(SecurityUtils.getLoginId()); // 设置登录通讯录Id
