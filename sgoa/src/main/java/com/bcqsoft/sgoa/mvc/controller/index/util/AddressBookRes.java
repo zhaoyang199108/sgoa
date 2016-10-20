ec9d2a920c3ea00359d5a2a4bce68b1545200e3f
Administrator <Administrator@ULOE3T20M9MZ3BS>
2016-10-20 10:41:41 +0800

    私人通讯录返回json类型定义

---
+++ sgoa/src/main/java/com/bcqsoft/sgoa/mvc/controller/index/util/AddressBookRes.java
@@ -0,0 +1,10 @@
+package com.bcqsoft.sgoa.mvc.controller.index.util;
+
+import java.util.List;
+
+import com.bcqsoft.sgoa.dao.addressbook.dataobject.AddressBook;
+
+public class AddressBookRes {
+	public String name;
+	public List< AddressBook> addressBook;
+}
