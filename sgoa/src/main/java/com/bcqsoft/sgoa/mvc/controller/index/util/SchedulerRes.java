58cd08bf0559f52b8e33fd9240f9bf962d573a73
zhaoyang <zhaoyang199108@163.com>
2016-10-24 14:05:38 +0800

    个人日程json类

---
+++ sgoa/src/main/java/com/bcqsoft/sgoa/mvc/controller/index/util/SchedulerRes.java
@@ -0,0 +1,10 @@
+package com.bcqsoft.sgoa.mvc.controller.index.util;
+
+import java.util.List;
+
+import com.bcqsoft.sgoa.dao.scheduler.dataobject.Scheduler;
+
+public class SchedulerRes {
+	public String date;
+	public List<Scheduler> scheduler;
+}
