a749fc1fa216b89ffd0e540ca2c7d080e4b4b5aa
zhaoyang <zhaoyang199108@163.com>
2016-10-24 10:34:07 +0800

    知识库接口

---
+++ sgoa/src/main/java/com/bcqsoft/sgoa/mvc/controller/index/AppResFileController.java
@@ -0,0 +1,78 @@
+package com.bcqsoft.sgoa.mvc.controller.index;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.bcqsoft.sgoa.core.security.SecurityUtils;
+import com.bcqsoft.sgoa.dao.resfile.dataobject.ResFile;
+import com.bcqsoft.sgoa.dao.resfile.dataobject.ResFilePage;
+import com.bcqsoft.sgoa.mvc.form.resfile.ResFileForm;
+import com.bcqsoft.sgoa.service.resfile.ResFileService;
+/*
+ * APP知识库接口
+ * */
+@Controller
+public class AppResFileController {
+	private @Autowired ResFileService resFileService;
+	/**
+	 * 所有上传文件的列表页面
+	 * 
+	 * @param
+	 * @param
+	 * @return
+	 * 
+	 * @Author zy
+	 * @Date 20161024
+	 */
+	@RequestMapping("/home/resFile/list.htm")
+	@ResponseBody
+	public Map<String,Object> resFileList(HttpServletRequest request,HttpServletResponse response) {
+		String currentPage = request.getParameter("currentPage");
+		String pageSize = request.getParameter("pageSize");
+		String loginId = request.getParameter("loginId");
+		String retCode = "";
+		String message = "";
+		if(currentPage == null||"".equals(currentPage)){
+			currentPage = "1";
+		}
+		if(pageSize == null||"".equals(pageSize)){
+			pageSize = "20";
+		}
+		ResFilePage resFilePage = new ResFilePage();
+		resFilePage.setCurrentPage(Integer.parseInt(currentPage));
+		resFilePage.setPageSize(Integer.parseInt(pageSize));
+		resFilePage.setLoginId(loginId);
+	//	setSearchKey(form, resFilePage); // 设置页面中的查询条件
+		// 取得上传页面列表,分页显示
+		ResFilePage page = resFileService.getResFileListByPage(resFilePage);
+		List<ResFile> list = page.getResFileList();
+		retCode = list==null?"1":"0";
+		message = list==null?"取得失败":"取得成功";
+		// 输出json格式数据
+		Map<String,Object> map = new HashMap<String,Object>();
+		map.put("ret_code", retCode);
+		map.put("message", message);
+		// 输出json格式数据
+		map.put("data",list);
+		return map;
+	}
+	private void setSearchKey(ResFileForm form, ResFilePage resFilePage) {
+		resFilePage.setCurrentPage(form.getCp()); // 当前页数
+		resFilePage.setTitle(form.getTitle());
+		resFilePage.setLoginId(SecurityUtils.getLoginId());
+		resFilePage.setSrcFileName(form.getSrcFileName());
+		resFilePage.setIsShore(form.getIsShore());
+		resFilePage.setUpdateDate(form.getUpdateDate());
+
+	}
+}
